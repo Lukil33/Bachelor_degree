63386694257226fd4d76132a1360bdfd04051e57
Lukil <lucprig33@gmail.com>
2025-04-27 20:25:34 +0200

    wip(competitiveprogramming): :construction: aggiunto esercizio hotel

--- 2_Anno/Competitive_programming/CSES/Hotel_queries/main.cpp
+++ 2_Anno/Competitive_programming/CSES/Hotel_queries/main.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 #define rep(i,a,b) for(int i=a; i<b; i++)
 
@@ -12,9 +14,8 @@ struct node{
 vector<int> vec;
 
 node* create_tree(int min, int max){
-    //cout<<"palle"<<endl;
     node* res = new node{0,0,nullptr,nullptr};
-    int mid = min + (max-min)/2;     
+    int mid = min + (max-min)/2;
     if(min == max){
         res->pos = min;
         if(min < (int)vec.size()){
@@ -29,25 +30,29 @@ node* create_tree(int min, int max){
 }
 
 int search_tree(node* nodo, int group){
+    int res;
     if(nodo->left == nullptr && nodo->right == nullptr){
-        return nodo->pos;
+        res = nodo->pos;
+	nodo->val -= group;
     }else if(nodo->val < group){
-        return -1;
+        res = -1;
     }else{
         if(nodo->left->val >= group){
-            return search_tree(nodo->left, group);
-        }else{
-            return search_tree(nodo->right, group);
+            res = search_tree(nodo->left, group);
+            nodo->val = max(nodo->left->val, nodo->right->val);
+	}else{
+            res = search_tree(nodo->right, group);
+	    nodo->val = max(nodo->left->val, nodo->right->val);
         }
     }
+    return res;
 }
 
 int main(){
-    ifstream in("input.txt");
-    ofstream out("output.txt");
+    // ifstream cin("input.txt");
 
     int N,M;
-    in>>N>>M;
+    cin>>N>>M;
 
     vec.resize(N);
 
@@ -57,20 +62,15 @@ int main(){
     }
 
     rep(i,0,N){
-        in>>vec[i];
+        cin>>vec[i];
     }
 
     node* tree = create_tree(0, l);
 
     rep(i,0,M){
         int a;
-        in>>a;
-        int modify = search_tree(tree, a);
-        if(modify != -1){
-            vec[modify] -= a;
-            tree = create_tree(0, l);
-        }
-        out<<modify+1<<" ";
+        cin>>a;
+        cout<<search_tree(tree, a)+1<<" ";
     }
 
     return 0;
