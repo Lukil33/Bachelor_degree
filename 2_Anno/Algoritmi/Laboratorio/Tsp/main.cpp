0c97983ff5bbec093a6b8c2aaa9b412079eaf635
Lukil <prigioneluca33@gmail.com>
2025-05-12 10:03:05 +0200

    fic(Algoritmi): :bug: migliorato esercizio Tsp

--- 2_Anno/Algoritmi/Laboratorio/Tsp/main.cpp
+++ 2_Anno/Algoritmi/Laboratorio/Tsp/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 #include <algorithm>
-//#include "tsp.h"
+#include "tsp.h"
 #define rep(i,a,b) for(int i=a; i<b; i++)
 using namespace std;
 
-vector<int> nodiCollegati;
 vector<vector<pair<int,int>>> listaAdiacenza; // si compone di: parte_da -> <peso,nodo_arrivo>, <peso,nodo_arrivo>,<peso,nodo_arrivo>
-int numNodiCollegati, numNodi;
+vector<vector<int>> matrice;
+vector<int> minusLowBound;
+int numNodi;
 
+int upperBound = -1;
 ofstream out ("output.txt");
 
-int greedysearch();
+void branchAndBound(int, int);
+void branchandbound_aux(vector<bool>&, vector<int>&, int, int);
+int lwBound();
 
 int main(){
     ifstream in ("input.txt");
 
     in >> numNodi;
 
-    nodiCollegati.resize(numNodi,0);
+    matrice.resize(numNodi, vector<int>(numNodi));
     listaAdiacenza.resize(numNodi);
 
     rep(i,1,numNodi){
@@ -28,6 +34,8 @@ int main(){
             in >> a;
             listaAdiacenza[j].push_back(make_pair(a,i));
             listaAdiacenza[i].push_back(make_pair(a,j));
+            matrice[i][j] = a;
+            matrice[j][i] = a;
         }
     }
 
@@ -35,39 +43,77 @@ int main(){
         sort(listaAdiacenza[i].begin(), listaAdiacenza[i].end());
     }
 
-    greedysearch();
+    int lowerBound = lwBound();
+
+    branchAndBound(upperBound, lowerBound);
 
     return 0;
 }
 
-int greedysearch(){
-    int costo=0;
+int lwBound(){
+    // Versione semplice, si può poi ottimizzare
+    minusLowBound.resize(numNodi,0);
+    int res = 0;
+    
+    unordered_map<int,set<int>> mappa;
+
+    rep(i,0,numNodi){
+        rep(j,0,numNodi-1){
+            if(mappa.find(listaAdiacenza[i][j].first) == mappa.end()){
+                mappa[listaAdiacenza[i][j].first] = {listaAdiacenza[i][j].second};
+                res += listaAdiacenza[i][j].first;
+                minusLowBound[i] = listaAdiacenza[i][j].first;
+                break;
+            }else if(mappa[listaAdiacenza[i][j].first].find(listaAdiacenza[i][j].second) == mappa[listaAdiacenza[i][j].first].end()){
+                mappa[listaAdiacenza[i][j].first].insert(listaAdiacenza[i][j].second);
+                res += listaAdiacenza[i][j].first;
+                minusLowBound[i] = listaAdiacenza[i][j].first;
+                break;
+            }else{
+                mappa[listaAdiacenza[i][j].first].erase(listaAdiacenza[i][j].second);
+            }
+        }
+    }
+
+    return res;
+}
+
+void branchAndBound(int uppBound, int lowBound){
     vector<bool> visited;
+    vector<int> stack;
 
     visited.resize(numNodi, false);
 
-    out<<0;
+    //per convenzione iniziamo sempre da 0 perche tanto è un ciclo e non cambia niente
     visited[0]=true;
-    int pos=0;
+    stack.push_back(0);
+    branchandbound_aux(visited, stack, 0, lowBound);
+}
 
-    rep(i,0,numNodi-1){
-        int min = 0;
-        while(min < numNodi-1 && visited[listaAdiacenza[pos][min].second]){
-            min += 1;
+void branchandbound_aux(vector<bool>& visited, vector<int>& stack, int costo, int lowBound){
+    if(upperBound == -1 || costo+lowBound < upperBound){
+        if((int)stack.size() == numNodi){
+            costo += matrice[stack.back()][0];
+            if(upperBound == -1 || costo < upperBound){
+                rep(i,0,numNodi){
+                    out<<stack[i]<<" ";
+                }
+                out<<stack[0]<<"#"<<endl;
+
+                upperBound=costo;
+            }
+        }else{
+            rep(i,0,(int)listaAdiacenza[stack.back()].size()){
+                int prec=stack.back();
+                int nextElem = listaAdiacenza[prec][i].second;
+                if(!visited[nextElem]){
+                    stack.push_back(nextElem);
+                    visited[nextElem]=true;
+                    branchandbound_aux(visited, stack, costo+matrice[prec][nextElem], lowBound-minusLowBound[prec]);
+                    visited[nextElem]=false;
+                    stack.pop_back();
+                }
+            }
         }
-        visited[listaAdiacenza[pos][min].second] = true;
-        costo += listaAdiacenza[pos][min].first;
-        out<<" "<<listaAdiacenza[pos][min].second;
-        pos = listaAdiacenza[pos][min].second;
     }
-
-    rep(i,0,numNodi-1){
-        if(listaAdiacenza[pos][i].second == 0){
-            costo += listaAdiacenza[pos][i].first;
-        }
-    }
-
-    out<<" "<<0<<"#"<<endl;
-
-    return costo;
 }
